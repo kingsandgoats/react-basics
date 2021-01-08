0ec7173522b7cd4056154ae5ec40829b9a87effd
kingsandgoats <pranishveliyath666@gmail.com>
2021-01-08 12:00:34 +0530

    Task completed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,58 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import "./style.css";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+console.log(FILTER_NAMES)
+
+
+
+
+
+
+
+
+
+
 function App() {
-  const [task, setTask] = useState([
+  const [tasks, setTask] = useState([
     { id: 1, name: "Eat", completed: true },
     { id: 2, name: "Sleep", completed: false },
     { id: 3, name: "Repeat", completed: false }
   ]);
 
+
+
+
+
+  const [filter,setFilter] = useState('All')
   const addTask = name => {
     const newTask = { id: Date.now(), name: name, completed: true };
     setTask([...task, newTask]);
   };
 
+
+    
+
+
+
+
+  const filterList = FILTER_NAMES.map(name => (
+      <FilterButton 
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}/>
+    ));
+
   const toggleCompleted = id => {
-    const updatedTasks = task.map(task => {
+    const updatedTasks = tasks.map(task => {
       if (id === task.id) {
         return { ...task, completed: !task.completed };
       }
@@ -28,22 +66,60 @@ function App() {
 
   const deleteTask = taskid => {
     console.log(taskid);
-    const remainingTask = task.filter(task => {
+    const remainingTask = tasks.filter(task => {
       console.log(task.id);
       return taskid !== task.id;
     });
     setTask(remainingTask);
     // console.log(remainingTask);
   };
-  const headingText = `${task.length} remaining`;
+
+  const editTask = (tasId,newName)=>{
+    console.log(tasId);
+    const editedTasks = tasks.map(task=>{
+      if(tasId === task.id){
+        return {...task,name:newName}
+      }
+      return task
+    })
+    setTask(editedTasks);
+  }
+
+
+  const headingText = `${tasks.length} remaining`;
+
+
+
+const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleCompleted={toggleCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
+
+
+
+
+
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form taskAdd={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton name="all" />
-        <FilterButton name="Active" />
-        <FilterButton name="all" />
+
+        {/*adding filter button*/}
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText} tasks remaining</h2>
       <ul
@@ -51,18 +127,7 @@ function App() {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        {...task.map(task => {
-          return (
-            <Todo
-              name={task.name}
-              completed={task.completed}
-              id={task.id}
-              key={task.id}
-              toggleCompleted={toggleCompleted}
-              deleteTask={deleteTask}
-            />
-          );
-        })}
+        {taskList}
       </ul>
     </div>
   );
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 
-class FilterButton extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+function FilterButton (props) {
     return (
-      <button type="button" className="btn toggle-btn" aria-pressed="true">
+      <button type="button" className="btn toggle-btn" aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}>
         <span className="visually-hidden">Show </span>
-        <span>{this.props.name}</span>
+        <span>{props.name}</span>
         <span className="visually-hidden"> tasks</span>
       </button>
     );
-  }
 }
 
 export default FilterButton;
